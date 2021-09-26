4c50c173c76a0b17abe68a60624b6c9517e394a6
omran <omranoon4@gmail.com>
2021-09-26 17:54:56 +0100

    Add COLORS variable, help and defining main function

--- CoolShell.py
+++ CoolShell.py
@@ -1,13 +1,52 @@
-import argparse
+#/bin/python3.8
+import argparse, sys, pprint
 from Colors import *
 
-def colorize(text:str, color=DEFAULT):
+def colorize(text:str, color:str=DEFAULT):
+    """colorize is function that return the styled a text argument and automatically add the default color at the end.
+    if no color is specified the color will be the default one"""
+
     return color + text + DEFAULT
 
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="make your shell colorized and cool".title())
+
+
+
+    parser.add_argument("-c", "--color", dest="color", required=True,
+                        type=str, metavar="color".upper(),
+                        help="which color you want to use.".title())
+
+    parser.add_argument("-t", "--text", dest="text", required=True,
+                        metavar="text".upper(), nargs="+", type=str,
+                        help="text to be colorized.".title())
+    
+    parser.add_argument("-n", "--no-newline", dest="no_newline", default="\n",
+                        required=False, const="", action="store_const",
+                        help="set it if you won't new line at the end of the text.".title())
+
+
+
+
+    options = parser.parse_args()
+    if options.color.upper() not in COLORS:
+        sys.stderr.write(colorize("[!] YOU SHOULD SELECT ONE OF THE COMING COLORS.\n", BOLD_RED))
+        pprint.pprint(COLORS, stream=sys.stderr)
+        return 2
+
+    USER_TEXT = " ".join(options.text)
+    USER_COLOR = eval(options.color.upper())
+    RESULT_TEXT = colorize(USER_TEXT, color=USER_COLOR)
+
+    print(RESULT_TEXT)
+    return 0
+    
+
+
+
+
 
 if __name__ == "__main__":
     main()
