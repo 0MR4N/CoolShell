ad403ffee5e2c897e4cf97161ffc92333521bd37
omran <omranoon4@gmail.com>
2021-09-26 16:36:03 +0100

    Fix colors, adding colorize function

---
+++ CoolShell.py
@@ -0,0 +1,13 @@
+import argparse
+from Colors import *
+
+def colorize(text:str, color=DEFAULT):
+    return color + text + DEFAULT
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
