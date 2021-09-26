addb0cc25fd12efe16fa71e12600c2e04cc3e450
omran <omranoon4@gmail.com>
2021-09-26 15:52:15 +0100

    adding gitignore, renaming color variables

--- Colors.py
+++ Colors.py
@@ -1,36 +1,41 @@
-# Regular Colors  
-Black="\[\033[0;30m\]" # Black
-Red="\[\033[0;31m\]" # Red
-Green="\[\033[0;32m\]" # Green
-Yellow="\[\033[0;33m\]" # Yellow
-Blue="\[\033[0;34m\]" # Blue
-Purple="\[\033[0;35m\]" # Purple
-Cyan="\[\033[0;36m\]" # Cyan
-White="\[\033[0;37m\]" # White
-# Bold
-BBlack="\[\033[1;30m\]" # Black
-BRed="\[\033[1;31m\]" # Red
-BGreen="\[\033[1;32m\]" # Green
-BYellow="\[\033[1;33m\]" # Yellow
-BBlue="\[\033[1;34m\]" # Blue
-BPurple="\[\033[1;35m\]" # Purple
-BCyan="\[\033[1;36m\]" # Cyan
-BWhite="\[\033[1;37m\]" # White
-# Underline
-UBlack="\[\033[4;30m\]" # Black
-URed="\[\033[4;31m\]" # Red
-UGreen="\[\033[4;32m\]" # Green
-UYellow="\[\033[4;33m\]" # Yellow
-UBlue="\[\033[4;34m\]" # Blue
-UPurple="\[\033[4;35m\]" # Purple
-UCyan="\[\033[4;36m\]" # Cyan
-UWhite="\[\033[4;37m\]" # White
-# Background
-On_Black="\[\033[40m\]" # Black
-On_Red="\[\033[41m\]" # Red
-On_Green="\[\033[42m\]" # Green
-On_Yellow="\[\033[43m\]" # Yellow
-On_Blue="\[\033[44m\]" # Blue
-On_Purple="\[\033[45m\]" # Purple
-On_Cyan="\[\033[46m\]" # Cyan
-On_White="\[\033[47m\]" # White
+RESET = "\[\e[0m\]"
+
+
+BLACK = "\[\033[0;30m\]"
+RED = "\[\033[0;31m\]"
+GREEN = "\[\033[0;32m\]"
+YELLOW = "\[\033[0;33m\]"
+BLUE = "\[\033[0;34m\]"
+PURPLE = "\[\033[0;35m\]"
+CYAN = "\[\033[0;36m\]"
+WHITE = "\[\033[0;37m\]"
+
+
+BOLD_BLACK = "\[\033[1;30m\]"
+BOLD_RED = "\[\033[1;31m\]"
+BOLD_GREEN = "\[\033[1;32m\]"
+BOLD_YELLOW = "\[\033[1;33m\]"
+BOLD_BLUE = "\[\033[1;34m\]"
+BOLD_PURPLE = "\[\033[1;35m\]"
+BOLD_CYAN = "\[\033[1;36m\]"
+BOLD_WHITE = "\[\033[1;37m\]"
+
+
+UNDERLINE_BLACK = "\[\033[4;30m\]"
+UNDERLINE_RED = "\[\033[4;31m\]"
+UNDERLINE_GREEN = "\[\033[4;32m\]"
+UNDERLINE_YELLOW = "\[\033[4;33m\]"
+UNDERLINE_BLUE = "\[\033[4;34m\]"
+UNDERLINE_PURPLE = "\[\033[4;35m\]"
+UNDERLINE_CYAN = "\[\033[4;36m\]"
+UNDERLINE_WHITE = "\[\033[4;37m\]"
+
+
+BACKGROUND_BLACK = "\[\033[40m\]"
+BACKGROUND_RED = "\[\033[41m\]"
+BACKGROUND_GREEN = "\[\033[42m\]"
+BACKGROUND_YELLOW = "\[\033[43m\]"
+BACKGROUND_BLUE = "\[\033[44m\]"
+BACKGROUND_PURPLE = "\[\033[45m\]"
+BACKGROUND_CYAN = "\[\033[46m\]"
+BACKGROUND_WHITE = "\[\033[47m\]"
